5aa75c41d5e100aa6da6e9c7988cb4ec76b57f7e
Neil RITTNER <neil.rittner@etu.univ-nantes.fr>
2018-07-30 10:11:37 +0100

    Modification: creation of server and first API

---
+++ Back/api/api.js
@@ -0,0 +1,53 @@
+"use strict";
+
+// npm dependencies and libraries
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const session = require('express-session');
+const utilities = require("../libraries/utilities");
+
+
+// Set the npm dependencies
+app.use(cors({ credentials: true, origin: 'http://localhost:4200' }));
+app.use(session({
+  secret: "Shh, its a secret!",
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Routing
+// Helloworld
+app.get("/helloworld", function (req, res, next) {
+  res.send("Hello World");
+});
+
+// TokenGoogle: used to verify a token
+app.post("/tokenGoogle", function (req, res) {
+  utilities.verify(req.body.idtoken).then(payload => {
+    session.user = payload;
+    res.send("OK");
+  })
+    .catch(console.error);
+});
+
+// Card: used to connect a user with his card
+app.post("/card", function (req, res) {
+  //Vérifier que ça existe en bdd + aller chercher l'utilisateur
+  session.user = {
+    given_name: "neil",
+    family_name: "rittner"
+  }
+  res.send();
+});
+
+// User: send the session's user (the connected user)
+app.get("/user", function (req, res) {
+  res.send(session.user);
+})
+
+module.exports = app;
---
+++ Back/server.js
@@ -0,0 +1,17 @@
+"use strict";
+
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const api = require("./api/api");
+const server = require("http").Server(app);
+
+app.use(express.static("public"));
+app.use("/static", express.static("public"));
+app.use("/api", api);
+
+app.get("/", function (req, res) {
+  res.send("Welcome on the root of the server");
+});
+
+server.listen(process.env.SERVER_PORT);
