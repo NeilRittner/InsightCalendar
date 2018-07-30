44b15bcefdf4f4a29e2063d2e0e0df7a655a6676
Neil RITTNER <neil.rittner@etu.univ-nantes.fr>
2018-07-30 11:12:52 +0100

    Modifications: to integrate the new components and routing

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { SocialLoginModule, AuthServiceConfig } from 'angular-6-social-login';
+import { getAuthServiceConfigs } from './socialloginConfig';
+import { AppRoutingModule, RoutingComponents } from './/app-routing.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoutingComponents
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocialLoginModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: getAuthServiceConfigs
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
