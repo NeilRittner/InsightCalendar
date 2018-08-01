d994a284696c815af8237c8f83e141081d16aecd
Neil RITTNER <neil.rittner@etu.univ-nantes.fr>
2018-08-01 16:16:05 +0100

    Modifications: include service

--- Front/src/app/login/login.component.ts
+++ Front/src/app/login/login.component.ts
@@ -1,8 +1,8 @@
 import { RemoteService } from './../remote.service';
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { AuthService, GoogleLoginProvider } from 'angular-6-social-login';
 import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -12,61 +12,41 @@ import { Router } from '@angular/router';
 
 export class LoginComponent implements OnInit {
   constructor(private socialAuthService: AuthService,
-    private http: HttpClient,
     private router: Router,
     private service: RemoteService
   ) { }
 
-  private idToken;
-  private idCard;
-  private idCardAsso;
+  private idCard: string;
 
   signinWithGoogle(): void {
     const socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
-    this.socialAuthService.signIn(socialPlatformProvider).then(
-      (userData) => {
-        this.sendToken(userData.idToken);
-      }
-    );
+    this.socialAuthService.signIn(socialPlatformProvider).then(userData => {
+      this.service.postGoogleToken(userData.idToken)
+        .subscribe((status: number) => {
+          if (status === 200) {
+            this.router.navigate(['/home']);
+          } else if (status === 500) {
+            // Print an error: internal error --> restart the process to sign in or try later
+          }
+        });
+    });
   }
 
   signinWithCard(): void {
-    this.http.post('http://localhost:8080/api/card', { withCredentials: true, idCard: this.idCard })
-      .subscribe((status: string) => {
-        this.router.navigate(['/home']);
-      }, onFail => {
-        // Do something to tell the user that his card is not store in the database
+    this.service.postCardId(this.idCard)
+      .subscribe((status: number) => {
+        if (status === 200) {
+          this.router.navigate(['/home']);
+        } else if (status === 404) {
+          // Print an error: card no found
+        } else {
+          // Print an error: internal error --> restart the process to sign in or try later
+        }
       });
   }
 
-  sendToken(token: string): void {
-    this.http.post('http://localhost:8080/api/tokenGoogle', { responseType: 'text', withCredentials: true, idToken: token })
-      .subscribe((onSuccess: string) => {
-        this.router.navigate(['/home']);
-      }, onFail => {
-        // Do something to tell the user that the connection fails
-      });
-  }
-
-  writeGoogleAccount(): void {
-    const socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
-
-    this.socialAuthService.signIn(socialPlatformProvider).then(
-      (userData) => { // On success
-        // This will return user data from google. What you need is a user token which you will send it to the server
-        this.idToken = userData.idToken;
-      }
-    );
-  }
-
-  scanCard() {
-    this.http.post('http://localhost:8080/api/associate',
-    { withCredentials: true, idToken: this.idToken, idCard: this.idCardAsso })
-      .subscribe((status: string) => {
-        this.router.navigate(['/home']);
-      }, onFail => {
-        // Do something to tell the user that his card is not store in the database
-      });
+  register(): void {
+    this.router.navigate(['/register']);
   }
 
   ngOnInit() {
