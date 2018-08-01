a22bff0a4f5e9a80beeef4b8f951c98af6780222
Neil RITTNER <neil.rittner@etu.univ-nantes.fr>
2018-08-01 16:17:37 +0100

    Modifications: connection and register a card (work)

--- Back/api/api.js
+++ Back/api/api.js
@@ -1,18 +1,19 @@
 "use strict";
 
 // npm dependencies and libraries
-const express = require("express");
+require('dotenv').config()
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const session = require('express-session');
-const connection = require("../libraries/connection");
+const connection = require('../libraries/connection');
 
 
 // Set the npm dependencies
 app.use(cors({ credentials: true, origin: 'http://localhost:4200' }));
 app.use(session({
-  secret: "Shh, its a secret!",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true
 }));
@@ -21,43 +22,81 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // Routing
-// Helloworld
-app.get("/helloworld", function (req, res, next) {
-  res.send("Hello World");
-});
 
-// TokenGoogle: used to verify a token
-app.post("/tokenGoogle", function (req, res) {
+/* Route: googleAccess
+* This route is used to verify the idToken sent by the client-side and set the session user.
+* Return a status code:
+* 200: OK
+* 500: Internal Error
+* 498: Token not valid
+*/
+app.post("/googleAccess", function (req, res) {
   connection.verifyGoogleToken(req.body.idToken).then(payload => {
     connection.getUserInformation(payload).then(user => {
       session.user = user;
       res.send('200');
-    });
+    })
+    .catch(err => {
+      res.send('500');
+    })
   })
-    .catch(console.error);
+  .catch(err =>  {
+    res.send('498');
+  });
 });
 
-// Card: used to connect a user with his card
-app.post("/card", function (req, res) {
-  // Vérifier que ça existe en bdd + aller chercher l'utilisateur
+/* Route: cardAccess
+* This route is used to verify the id of the card sent by the client-side and set the session user.
+* Return a status code:
+* 200: OK
+* 404: User not find
+* 500: Internal Error
+*/
+app.post("/cardAccess", function (req, res) {
   connection.verifyCard(req.body.idCard).then(user => {
     if (user) {
+      console.log(user);
       session.user = user;
       res.send('200');
     }
     else {
       res.send('404');
     }
+  })
+  .catch(err => {
+    res.send('500');
   });
 });
 
-app.post("/associate", function (req, res) {
-  console.log(req.body.idToken);
-  console.log(req.body.idCard);
+/* Route: registerCard
+* This route is used to link an idCard to a user (for the IoT Access enabled).
+* Return a status code:
+* 200: OK
+* 500: Internal Error
+* 498: Token not valid
+*/
+app.post("/registerCard", function (req, res) {
+  connection.verifyGoogleToken(req.body.idToken).then(payload => {
+    connection.registerCard(payload, req.body.idCard).then(user => {
+      console.log('User api:   ' + user);
+      session.user = user;
+      res.send('200');
+    })
+    .catch(err => {
+      res.send('500');
+    });
+  })
+  .catch(err => {
+    res.send('498');
+  });
 })
 
-// User: send the session's user (the connected user)
-app.get("/user", function (req, res) {
+/* Route: currentUser
+* This route is used to send information about the currentUser to the client-side.
+* Return a json object as :
+* { 'IdGoogle':, 'LastName':, 'FirstName':, 'Email': }
+*/
+app.get("/currentUser", function (req, res) {
   res.send(session.user);
 })
 
--- Back/libraries/connection.js
+++ Back/libraries/connection.js
@@ -10,13 +10,10 @@ const pool = require("../db/pool");
 module.exports = {
   verifyGoogleToken: async function (token) {
     const client = new OAuth2Client(process.env.CLIENT_ID);
-
-    // Verify the token
     const ticket = await client.verifyIdToken({
       idToken: token,
       audience: process.env.CLIENT_ID,
     });
-
     const payload = ticket.getPayload();
     return payload;
   },
@@ -29,9 +26,10 @@ module.exports = {
             resolve(data2);
           });
         } 
-        else {
-          resolve(data[0]);
-        }
+        else resolve(data[0]);
+      })
+      .catch(err => {
+        reject(err);
       });
     })
   },
@@ -41,58 +39,92 @@ module.exports = {
 
     return new Promise((resolve, reject) => {
       pool.calendar_pool.query(query, function (err, row) {
-        if (!err) {
-          resolve(row);
-        }
+        if (!err) resolve(row);
         else reject(err);
       });
     });
   },
 
-
-  insertNewUser: function (googleUserInfo) {
+  insertNewUser: function (googleUserInfo, idCard = null) {
     const googleId = googleUserInfo['sub'];
-    const fName = '' + googleUserInfo['family_name'];
-    const gName = '' + googleUserInfo['given_name'];
-    const email = ''+ googleUserInfo['email'].substring(0, googleUserInfo['email'].indexOf('@'));
-
+    const fName = googleUserInfo['family_name'];
+    const gName = googleUserInfo['given_name'];
+    const email = googleUserInfo['email'].substring(0, googleUserInfo['email'].indexOf('@'));
     const user = {
       'IdGoogle': googleId,
       'LastName': fName,
       'FirstName': gName,
       'Email': email
     };
-    
-    const query = `INSERT 
-    INTO users (IdGoogle, LastName, FirstName, Email) 
-    VALUES ('${ googleId }', '${ fName }', '${ gName }', '${ email }')`;
+    let query = ``;
+
+    if(idCard !== null) {
+      query = `INSERT INTO users 
+      (IdGoogle, LastName, FirstName, Email, IdCard) 
+      VALUES ('${ googleId }', '${ fName }', '${ gName }', '${ email }', '${ idCard }')`;
+    }
+    else {
+      query = `INSERT INTO users 
+      (IdGoogle, LastName, FirstName, Email) 
+      VALUES ('${ googleId }', '${ fName }', '${ gName }', '${ email }')`;
+    }
 
     return new Promise((resolve, reject) => {
       pool.calendar_pool.query(query, function (err) {
+        if (!err) resolve(user);
+        else reject(err);
+      });
+    });
+  },
+
+  verifyCard: function (idCard) {
+    const query = `SELECT * FROM users WHERE IdCard = ${ idCard }`;
+
+    return new Promise((resolve, reject) => {
+      pool.calendar_pool.query(query, function (err, rows) {
         if (!err) {
-          resolve(user);
+          if (rows.length) resolve(rows[0]);
+          else resolve(null);
         }
         else reject(err);
       });
     });
   },
 
-  verifyCard: function (cardNumber) {
-    const query = `SELECT * FROM users WHERE IdAccessCard = ${cardNumber}`;
-
+  registerCard: function (googleUserInfo, idCard) {
     return new Promise((resolve, reject) => {
-      pool.calendar_pool.query(query, function (err, rows) {
-        if (!err) {
-          if (rows.length) {
-            resolve(rows);
-          }
-          else {
-            resolve(null);
-          }
+      this.googleUserExist(googleUserInfo['sub']).then(data => {
+        if (data.length === 0) {
+          this.insertNewUser(googleUserInfo, idCard).then(data2 => {
+            resolve(data2);
+          })
+          .catch(err => {
+            reject(err);
+          });
         }
-        else reject(err);
+        else {
+          this.updateCard(googleUserInfo['sub'], idCard).then(data3 => {
+            resolve(data[0]);
+          })
+          .catch(err => {
+            reject(err);
+          });
+        }
+      })
+      .catch(err => {
+        reject(err);
       });
     });
+  },
+
+  updateCard: function (googleUserId, idCard) {
+    const query = `UPDATE users SET IdCard = ${idCard} WHERE IdGoogle = ${googleUserId}`;
+    return new Promise ((resolve, reject) => {
+      pool.calendar_pool.query(query, function (err, result) {
+        if (!err) resolve('row updated');
+        else reject(err);
+      });
+    })
   }
 
 }
