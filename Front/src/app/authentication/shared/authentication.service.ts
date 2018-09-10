cf9ac6a32886237c81e3dd1a4c66f5de9ed60fa6
Neil RITTNER <neil.rittner@etu.univ-nantes.fr>
2018-09-10 14:50:36 +0100

    Modifications: add headers to request

--- Front/src/app/authentication/shared/authentication.service.ts
+++ Front/src/app/authentication/shared/authentication.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
@@ -16,7 +16,9 @@ export class AuthenticationService {
   postGoogleToken(token: string): Observable<any> {
     const url = `${environment.serveur_url}api/googleAccess`;
     const httpBody = { idToken: token };
-    const httpOptions = { withCredentials: true };
+    const httpHeader = new HttpHeaders();
+    httpHeader.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    const httpOptions = { headers: httpHeader, withCredentials: true };
     return this.http
       .post(url, httpBody, httpOptions)
       .pipe(catchError(err => throwError(err)));
@@ -24,17 +26,21 @@ export class AuthenticationService {
 
   postCardId(idCard: string) {
     const url = `${environment.serveur_url}api/cardAccess`;
-    const httpBody = { idCard: idCard, observe: 'response' };
-    const httpOptions = { withCredentials: true };
+    const httpHeader = new HttpHeaders();
+    httpHeader.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    const httpBody = { idCard: idCard };
+    const httpOptions = { headers: httpHeader, withCredentials: true };
     return this.http
-      .post(url, httpOptions, { withCredentials: true })
+      .post(url, httpBody, httpOptions)
       .pipe(catchError(err => throwError(err)));
   }
 
   postRegisterCard(token: string, idCard: string) {
     const url = `${environment.serveur_url}api/registerCard`;
     const httpBody = { idToken: token, idCard: idCard };
-    const httpOptions = { withCredentials: true };
+    const httpHeader = new HttpHeaders();
+    httpHeader.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    const httpOptions = { headers: httpHeader, withCredentials: true };
     return this.http
       .post(url, httpBody, httpOptions)
       .pipe(catchError(err => throwError(err)));
@@ -43,7 +49,9 @@ export class AuthenticationService {
   postCode(code: string) {
     const url = `${environment.serveur_url}api/code`;
     const httpBody = { code: code };
-    const httpOptions = { withCredentials: true };
+    const httpHeader = new HttpHeaders();
+    httpHeader.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    const httpOptions = { headers: httpHeader, withCredentials: true };
     return this.http
       .post(url, httpBody, httpOptions)
       .pipe(catchError(err => throwError(err)));
